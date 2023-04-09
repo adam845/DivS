ada88c79ba6143ca399b8c5c268688224d758ad9
Adam Khalil <adam.khalil.2002@icloud.com>
2023-04-09 15:03:32 +0200

    d

--- home/serializers.py
+++ home/serializers.py
@@ -4,9 +4,7 @@ from news.serializers import NewsSerializer
 from .models import Home
 
 class HomeSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True)
-    news = NewsSerializer(many=True)
-
+ 
     class Meta:
         model = Home
-        fields = ['logo_image', 'logo_alt_image', 'products', 'news']
+        fields = '__all__'
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Home
@@ -9,6 +10,15 @@ from news.serializers import NewsSerializer
 
 @api_view(['GET'])
 def home_page(request):
-    home = Home.objects.all().first()
-    serializer = HomeSerializer(home)
-    return Response(serializer.data)
+    home = Home.objects.first()  # retrieve a single instance of Home
+    products = Product.objects.all()
+    news = News.objects.all()
+    home_serializer = HomeSerializer(home)
+    product_serializer = ProductSerializer(products, many=True)
+    news_serializer = NewsSerializer(news, many=True)
+    data = {
+        'home': home_serializer.data,
+        'products': product_serializer.data,
+        'news': news_serializer.data
+    }
+    return Response(data)
